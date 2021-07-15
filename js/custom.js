c24be214475b227c03c7ab4bfedf82c21299e1c6
Ady Juliyanto <adyjuliyanto@webmail.umm.ac.id>
2021-07-16 02:16:52 +0700

    fixing design and js

--- js/custom.js
+++ js/custom.js
@@ -7,45 +7,6 @@
   //   console.log("kk");
   // }
 
-  // $('.scroll-to-section').on('click',function(e){
-  // 	var tujuan = $this.attr('href');
-  // 	var elementTujuan = $(tujuan);
-  // 	$('body').animate({
-  // 		scrollTop : elementTujuan.offset().top-50
-  // 	},1000,'easeInOutExpo')
-  // })
-
-  var elementWindow = $(document).width();
-  if(979 >= elementWindow){
-    $(".navbar").removeClass("bg-transparent");
-    $(".navbar").addClass("bg-white");
-    $(".nav-link").removeClass("text-white");
-  }else{
-    $(".navbar").removeClass("bg-white");
-    $(".navbar").addClass("bg-transparent");
-    $(".nav-link").addClass("text-white");   
-  }
-
-
-  $(window).resize(function(){    
-    // console.log($(document).width()) 
-    var element = $(document).width();    
-    if(979 >= element){
-      $(".navbar").removeClass("bg-transparent");
-      $(".navbar").addClass("bg-white");  
-      $(".nav-link").removeClass("text-white");
-    }else{
-      $(".navbar").removeClass("bg-white");
-      $(".navbar").addClass("bg-transparent");  
-      $(".nav-link").addClass("text-white");  
-    }
-  })  
-
-  // var id = $(".navbar-toggler")
-
-  // console.log(id.attr('aria-expanded'))
-
-  
   var scrollLink = $(".scroll-to-section");
   scrollLink.click(function (e) {
     e.preventDefault();
@@ -60,14 +21,55 @@
 
   $(window).scroll(function () {
     var scroll = $(window).scrollTop();
-    var box = $(".hero-slides").height() - 20;
+    var box = $(".jumbotron").height() - 100;
     var header = $(".navbar").height();
 
     if (scroll >= box - header) {
-      $(".navbar").addClass("fixed-top");      
+      $(".navbar").addClass("fixed-top");
     } else {
       $(".navbar").removeClass("fixed-top");
     }
+  });
+
+  // var elementWindow = $(document).width();
+  // if (979 >= elementWindow) {
+  //   $(".navbar").removeClass("bg-transparent");
+  //   $(".navbar").addClass("bg-white");
+  //   $(".nav-link").removeClass("text-white");
+  // } else {
+  //   $(".navbar").removeClass("bg-white");
+  //   $(".navbar").addClass("bg-transparent");
+  //   $(".nav-link").addClass("text-white");
+  // }
+
+  // $(window).resize(function () {
+  //   // console.log($(document).width())
+  //   var element = $(document).width();
+  //   if (979 >= element) {
+  //     $(".navbar").removeClass("bg-transparent");
+  //     $(".navbar").addClass("bg-white");
+  //     $(".nav-link").removeClass("text-white");
+  //   } else {
+  //     $(".navbar").removeClass("bg-white");
+  //     $(".navbar").addClass("bg-transparent");
+  //     $(".nav-link").addClass("text-white");
+  //   }
+  // });
+
+  // var id = $(".navbar-toggler")
+
+  // console.log(id.attr('aria-expanded'))
+
+  var scrollLink = $(".scroll-to-section");
+  scrollLink.click(function (e) {
+    e.preventDefault();
+    $("body,html").animate(
+      {
+        scrollTop: $(this.hash).offset().top - 70,
+      },
+      1000,
+      "easeInOutExpo"
+    );
   });
 
   $(".berita").owlCarousel({
@@ -114,14 +116,10 @@
   });
 
   // Window Resize Mobile Menu Fix
-  
 
   // Scroll animation init
-  
 
   // Window Resize Mobile Menu Fix
- 
 
   // Window Resize Mobile Menu Fix
-  
 })(window.jQuery);
